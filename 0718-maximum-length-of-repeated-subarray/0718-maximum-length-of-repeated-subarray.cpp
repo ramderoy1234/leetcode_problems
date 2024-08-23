dfd920abf0020b3fd288237e4cc475a55b327457
ramderoy1234 <moodramcharan@gmail.com>
2024-08-23 23:49:43 +0530

    Time: 89 ms (52.79%), Space: 88.4 MB (7.91%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,31 +1,45 @@
 class Solution {
-    int recursion(vector<int>& nums1, vector<int>& nums2, int n, int m, vector<vector<int>>& dp, int& maxLength) {
-        if (n == 0 || m == 0) return 0; 
+    // int recursion(vector<int>& nums1, vector<int>& nums2, int n, int m, vector<vector<int>>& dp, int& maxLength) {
+    //     if (n == 0 || m == 0) return 0; 
 
-        if (dp[n][m] != -1) return dp[n][m];  
+    //     if (dp[n][m] != -1) return dp[n][m];  
 
-        if (nums1[n - 1] == nums2[m - 1]) {
-            dp[n][m] = 1 + recursion(nums1, nums2, n - 1, m - 1, dp, maxLength);
-            maxLength = max(maxLength, dp[n][m]); 
-        } else {
-            dp[n][m] = 0;  
-        }
+    //     if (nums1[n - 1] == nums2[m - 1]) {
+    //         dp[n][m] = 1 + recursion(nums1, nums2, n - 1, m - 1, dp, maxLength);
+    //         maxLength = max(maxLength, dp[n][m]); 
+    //     } else {
+    //         dp[n][m] = 0;  
+    //     }
 
-        return dp[n][m];
-    }
+    //     return dp[n][m];
+    // }
 
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); 
-        int maxLength = 0; 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                recursion(nums1, nums2, i, j, dp, maxLength);
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); 
+        // int maxLength = 0; 
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         recursion(nums1, nums2, i, j, dp, maxLength);
+        //     }
+        // }
+        // return maxLength; 
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       
+        int maxi=0;
+        for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+            if(nums1[i-1]==nums2[j-1]){
+              dp[i][j]=dp[i-1][j-1]+1;
+              maxi=max(maxi,dp[i][j]);
             }
+            else{
+              dp[i][j]=0;
+            }
+          }
         }
-
-        return maxLength; 
+        return maxi;
     }
 };
