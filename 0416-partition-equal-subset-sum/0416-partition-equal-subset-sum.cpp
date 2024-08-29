b2cfd318283dbf20f7ba4d2aebaa342337d9fbc5
ramderoy1234 <moodramcharan@gmail.com>
2024-08-29 23:30:12 +0530

    Time: 16 ms (98.74%), Space: 99.7 MB (21.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,26 @@
 class Solution {
-    // bool recursion(vector<int>& nums, int sum, int n,vector<vector<int>>&dp) {
-    //     if (sum == 0) return true; 
-    //     if (n == 0 || sum < 0) return false; 
-    //     if(dp[n][sum]!=-1) return dp[n][sum];
-    //     return dp[n][sum]=recursion(nums, sum - nums[n - 1], n - 1,dp) || recursion(nums, sum, n - 1,dp);
-    // }
-    
+int recursion(int n,vector<int>&nums,int tar_sum,vector<vector<int>>&dp){
+    if(n==0) return 0;
+    if(tar_sum==0) return 1;
+    if(dp[n][tar_sum]!=-1) return dp[n][tar_sum];
+    if(nums[n-1]<=tar_sum){
+      dp[n][tar_sum]=recursion(n-1,nums,tar_sum-nums[n-1],dp)|| recursion(n-1,nums,tar_sum,dp);
+    }
+    else{
+      dp[n][tar_sum]=recursion(n-1,nums,tar_sum,dp);
+    }
+    return dp[n][tar_sum];
+}  
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % 2 != 0) return false;
-
-        int sum_tar = sum / 2;
-        vector<vector<bool>> dp(n + 1, vector<bool>(sum_tar + 1, false));
-        for (int i = 0; i <= n; ++i) {
-            dp[i][0] = true;
+        int sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+          sum+=nums[i];
         }
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= sum_tar; ++j) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        return dp[n][sum_tar];
+        if(sum%2!=0) return false;
+        int tar_sum=sum/2;
+        vector<vector<int>>dp(n+1,vector<int>(tar_sum+1,-1));
+        return recursion(n,nums,tar_sum,dp);
     }
 };
