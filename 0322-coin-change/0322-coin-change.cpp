0390445ea7254d451c3aecf5a23af7bbfff5f775
ramderoy1234 <moodramcharan@gmail.com>
2024-08-30 14:43:01 +0530

    Time: 43 ms (42.08%), Space: 52.4 MB (8.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    int recursion(const vector<int>& coins, int amount, int n, vector<vector<int>>& dp) {
-        if (amount == 0) return 0;
-        if (n == 0) return INT_MAX - 1;
-        if (dp[n][amount] != -1) return dp[n][amount];
-        int exclude = recursion(coins, amount, n-1, dp);
-        int include = INT_MAX;
-        if (coins[n-1] <= amount) {
-            include = recursion(coins, amount - coins[n-1], n, dp);
-            if (include != INT_MAX) include += 1;
-        }
-
-        dp[n][amount] = min(exclude, include);
-        return dp[n][amount];
+  int recursion(int n,int amount,vector<int>&nums,vector<vector<int>>&dp){
+    if(n==0) return INT_MAX-1;
+    if(amount==0) return 0;
+    if(dp[n][amount]!=-1) return dp[n][amount];
+    int exclude=recursion(n-1,amount,nums,dp);
+    int include=INT_MAX;
+    if(nums[n-1]<=amount){
+      include=recursion(n,amount-nums[n-1],nums,dp);
+      if(include!=INT_MAX-1) include+=1;
     }
+    int mini=min(include,exclude);
+   return  dp[n][amount]=mini;
+  }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
-        int result = recursion(coins, amount, n, dp);
-        return result == INT_MAX - 1 ? -1 : result;
+        int n=coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+       int ans=recursion(n,amount,coins,dp);
+       return ans==INT_MAX-1?-1:ans; 
+
     }
 };
