49ea8193a247b77fc586b099539e079bc6a4540a
ramderoy1234 <moodramcharan@gmail.com>
2024-09-05 11:39:05 +0530

    Time: 0 ms (100%), Space: 9.5 MB (44.37%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,11 +1,11 @@
 class Solution {
 int  f(vector<int>&nums ,int n,vector<int>&dp){
-  if(n==0) return 0;
-  if(n==1) return nums[0];
-  if(dp[n]!=-1) return dp[n];
-  int skip=f(nums,n-1,dp);
-  int add=nums[n-1]+f(nums,n-2,dp);
-  return dp[n]=max(skip,add);
+   if(n==0) return 0;
+   if(n==1) return nums[0];
+   if(dp[n]!=-1) return dp[n];
+   int skip=f(nums,n-1,dp);
+   int add=nums[n-1]+f(nums,n-2,dp);
+   return dp[n]=max(skip,add);
 }
 public:
     int rob(vector<int>& nums) {
