2803934d9baee0892913cadb63434b108c7b9978
ramderoy1234 <moodramcharan@gmail.com>
2024-09-09 15:52:06 +0530

    Time: 35 ms (61.44%), Space: 31.7 MB (24.45%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  int recursion(int n, int m, string &s, string &p, vector<vector<int>>& dp) {
+    if (m == 0) return n == 0;  // Only match if both string and pattern are empty
+    if (n == 0) {
+      // If string is empty but pattern is not, it can only match if pattern has only '*' left
+      for (int i = 0; i < m; ++i) {
+        if (p[i] != '*') return 0;
+      }
+      return 1;
+    }
+    
+    if (dp[n][m] != -1) return dp[n][m];
+
+    if (p[m-1] == s[n-1] || p[m-1] == '?') {
+      dp[n][m] = recursion(n-1, m-1, s, p, dp); 
+    }
+     else if (p[m-1] == '*') {
+      dp[n][m] = recursion(n-1, m, s, p, dp) || recursion(n, m-1, s, p, dp); 
+    }
+     else {
+      dp[n][m] = 0; 
+    }
+
+    return dp[n][m];
+  }
+  
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return recursion(n, m, s, p, dp);
+    }
+};
