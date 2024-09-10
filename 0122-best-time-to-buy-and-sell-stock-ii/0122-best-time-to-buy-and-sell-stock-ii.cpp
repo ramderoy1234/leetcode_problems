10bb335b3e4bb59dc022ec37daa49ee595398b27
ramderoy1234 <moodramcharan@gmail.com>
2024-09-10 12:18:27 +0530

    Time: 13 ms (9.13%), Space: 20.6 MB (9.04%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
-    int recursion(int index, int buy, vector<int>& prices, vector<vector<int>>& dp) {
-        if(index >= prices.size()) return 0;
-        if(dp[index][buy]!=-1) return dp[index][buy];
-        
-        int notpick=recursion(index+1,buy,prices,dp);
-        if(buy){
-          int sell=prices[index]+recursion(index+1,0,prices,dp);
-          dp[index][buy]=max(notpick,sell);
-        }
-        else{
-          int buyStocks=-prices[index]+recursion(index+1,1,prices,dp);
-          dp[index][buy]=max(notpick,buyStocks);
-        }
-        return dp[index][buy];
+  int recursion(int index,int buy, vector<int>&arr,vector<vector<int>>&dp){
+    if(index >= arr.size()) return 0;
+    if(dp[index][buy]!=-1) return dp[index][buy];
+    int notpick=recursion(index+1,buy,arr,dp);
+    int maxi=0;
+    if(buy==1){
+      int sell=arr[index]+recursion(index+1,0,arr,dp);
+      maxi=max(sell,notpick);
     }
-
+    else{
+      int buy_today=-arr[index]+recursion(index+1,1,arr,dp);
+      maxi=max(notpick,buy_today);
+    }
+    return dp[index][buy]=maxi;
+  }
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return recursion(0, 0, prices, dp);
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return recursion(0,0,prices,dp);
     }
 };
