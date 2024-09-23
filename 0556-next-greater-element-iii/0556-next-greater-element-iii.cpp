ec12ecbb64fbe3d00ac29e16432a765d34d5a37a
ramderoy1234 <moodramcharan@gmail.com>
2024-09-23 21:34:21 +0530

    Time: 0 ms (100%), Space: 7.5 MB (62.48%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -1,45 +1,29 @@
 class Solution {
 public:
     int nextGreaterElement(int n) {
-    string s = to_string(n);
-    n = s.size();
-    bool ok = 0;
-    for(int i = n - 1; i >= 0; i--){
-         for(int j = i + 1; j < n; j++){
-             if(s[j] > s[i]){
-                ok = 1;
-             }
-         }
-         if(ok){
-            char x = '~';
-            int p = -1;
-            for(int j = i + 1; j < n; j++){
-                 if(s[j] > s[i] && x > s[j]){
-                    x = s[j];
-                    p = j;
-                 }
-            }
-            swap(s[p], s[i]);
-            if(p < n){
-            sort(s.begin() + i + 1,s.end());
-            }
-            break;
-         }
-    }
-    if(ok ^ 1){
-         return -1;
-    }
-    long long ans = 0;
-    long long p = 1;
-    // cout << s << '\n';
-    for(int i = s.size() - 1; i >= 0; i--){
-         ans += p * 1LL * (s[i] - '0');
-         p *= 10;
-    }
-    
-    if(ans > 1LL * INT_MAX){
-         return -1;
-    }
-    return ans;
+        string s = to_string(n);
+        int len = s.size();
+
+        int i = len - 2;
+        while (i >= 0 && s[i] >= s[i + 1]) {
+            i--;  // found less element
+        }
+
+        if (i < 0) {
+            return -1;
+        }
+
+        // Step 2: Find the smallest digit on right side of i that is larger than s[i]
+        int j = len - 1;
+        while (s[j] <= s[i]) {
+            j--;
+        }
+        // Step 3: Swap digits at i and j
+        swap(s[i], s[j]);
+
+        reverse(s.begin() + i + 1, s.end());
+        long long result = stoll(s);
+
+        return (result > INT_MAX) ? -1 :result; 
     }
 };
