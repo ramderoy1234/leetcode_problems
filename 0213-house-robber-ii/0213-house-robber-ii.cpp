0993e12074e25d3e25a4bc3c1fa01a9a8093a2c1
ramderoy1234 <moodramcharan@gmail.com>
2024-09-24 21:15:31 +0530

    Time: 0 ms (100%), Space: 9.7 MB (36.96%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,21 @@
 class Solution {
-  int recursion(int n,int start, vector<int>&nums,vector<int>&dp){
+  int recursion(int n,int start,vector<int>&nums,vector<int>&dp){
     if(n<start) return 0;
     if(n==start) return nums[start];
     if(dp[n]!=-1) return dp[n];
-    int no=recursion(n-1,start,nums,dp);
+    int skip=recursion(n-1,start,nums,dp);
     int add=nums[n]+recursion(n-2,start,nums,dp);
-    return dp[n]=max(no,add);
+    return dp[n]=max(add,skip);
   }
 public:
     int rob(vector<int>& nums) {
-      int n=nums.size();
-      if(n==1) return nums[0];
-      vector<int>dp1(n+1,-1);
-      vector<int>dp2(n+1,-1);
-      int no=recursion(n-1,1,nums,dp1);
-      int add=recursion(n-2,0,nums,dp2);
-      return max(no,add);
+        int n=nums.size();
+        if(n==1) return nums[0];
+        vector<int>dp1(n+1,-1);
+        vector<int>dp2(n+1,-1);
+        int no=recursion(n-2,0,nums,dp1);
+        int add=recursion(n-1,1,nums,dp2);
+
+        return max(add,no);
     }
 };
