9c9ea181775f98b9ce845bcac53f44e5348637ca
ramderoy1234 <moodramcharan@gmail.com>
2024-09-24 21:00:21 +0530

    Time: 0 ms (100%), Space: 9.5 MB (64.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,16 @@
 class Solution {
-int  f(vector<int>&nums ,int n,vector<int>&dp){
-   if(n==0) return 0;
-   if(n==1) return nums[0];
-   if(dp[n]!=-1) return dp[n];
-   int skip=f(nums,n-1,dp);
-   int add=nums[n-1]+f(nums,n-2,dp);
-   return dp[n]=max(skip,add);
-}
+  int recursion(int n, vector<int>&nums,vector<int>&dp){
+    if(n==0) return 0;
+    if(n==1) return nums[0];
+    if(dp[n]!=-1) return dp[n];
+    int skip=recursion(n-1,nums,dp);
+    int add=nums[n-1]+recursion(n-2,nums,dp);
+    return dp[n]=max(add,skip);
+  }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n+1,-1);
-        return f(nums,n,dp);
+        return recursion(n,nums,dp);
     }
 };
