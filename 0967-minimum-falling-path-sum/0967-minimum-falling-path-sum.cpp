8b6e24ea613b50cae88524012c78acdc67eb674a
ramderoy1234 <moodramcharan@gmail.com>
2024-09-25 21:58:48 +0530

    Time: 9 ms (79.88%), Space: 13.7 MB (78.98%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        if (n == 0) return 0;
-        vector<vector<int>> dp = matrix;
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = 0; j < n; ++j) {
-                int down = dp[i + 1][j];  // Directly below
-                int left = (j > 0) ? dp[i + 1][j - 1] : INT_MAX;  // Diagonally left below
-                int right = (j < n - 1) ? dp[i + 1][j + 1] : INT_MAX;  // Diagonally right below
-
-                dp[i][j] +=min({down, left, right});
-            }
+        int n=matrix.size();
+        if(n==0) return 0;
+        vector<vector<int>>dp=matrix;
+        for(int i=n-2;i>=0;i--){
+          for(int j=0;j<n;j++){
+            int down=dp[i+1][j];
+            int leftDia=(j>0)?dp[i+1][j-1]:INT_MAX;
+            int rightDia=(j<n-1)?dp[i+1][j+1]:INT_MAX;
+            dp[i][j]+=min({down,leftDia,rightDia});
+          }
         }
-
-        // int min_path = INT_MAX;
-        // for (int j = 0; j < n; ++j) {
-        //     min_path= std::min(min_path, dp[0][j]);
-        // }
-
-        return *min_element(dp[0].begin(),dp[0].end());
+        int mini=INT_MAX;
+        for(int j=0;j<n;j++){
+          mini=min(mini,dp[0][j]);
+        }
+        return mini;
     }
 };
