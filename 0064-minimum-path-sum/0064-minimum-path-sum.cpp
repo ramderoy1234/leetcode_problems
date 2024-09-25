02f6e4cfea90b1e9b0774b68686f18dd287129e9
ramderoy1234 <moodramcharan@gmail.com>
2024-09-25 11:45:12 +0530

    Time: 14 ms (16.91%), Space: 14.9 MB (13.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
-int recursion(int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp){
-  if(n==0 &&m==0) return grid[0][0];
-  if(n<0 || m<0) return INT_MAX;
-  if(dp[n][m]!=-1) return dp[n][m];
-  int down =recursion(n-1,m,grid,dp);
-  int right=recursion(n,m-1,grid,dp);
-  return dp[n][m]=grid[n][m]+min(down,right);
-}  
+  int recursion(int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp){
+    if(n==0 && m==0) return grid[0][0];
+    if(n<0 || m<0) return INT_MAX;
+    if(dp[n][m]!=-1) return dp[n][m];
+    int left=recursion(n-1,m,grid,dp);
+    int right=recursion(n,m-1,grid,dp);
+    return dp[n][m]=grid[n][m]+min(left,right);
+  }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return recursion(n-1,m-1,grid,dp);
+      int n=grid.size();
+      int m=grid[0].size();
+      vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+      return recursion(n-1,m-1,grid,dp);
     }
 };
