a32751c5a5758d61b9032518ba1ff38e1cea0753
ramderoy1234 <moodramcharan@gmail.com>
2024-10-06 13:51:14 +0530

    Time: 25 ms (39.24%), Space: 60.6 MB (95.05%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,37 +1,72 @@
-
 class Solution {
-  private:
-  TreeNode*first;
-  TreeNode*prev;
-  TreeNode*middle;
-  TreeNode*last;
-  void inorder(TreeNode*root){
-    if(root==nullptr){
-      return ;
+private:
+    TreeNode* first;
+    TreeNode* prev;
+    TreeNode* middle;
+    TreeNode* last;
+
+    // Morris inorder traversal
+    void morrisTraversal(TreeNode* root) {
+        TreeNode* curr = root;
+        while (curr != nullptr) {
+            if (curr->left == nullptr) {
+                // Process the current node and detect swapped nodes
+                if (prev != nullptr && curr->val < prev->val) {
+                    if (first == nullptr) {
+                        first = prev;
+                        middle = curr;
+                    } else {
+                        last = curr;
+                    }
+                }
+                prev = curr;
+                curr = curr->right; // Move to the right subtree
+            }
+            
+             else {
+                // Find the inorder predecessor of curr
+                TreeNode* pred = curr->left;
+                while (pred->right != nullptr && pred->right != curr) {
+                    pred = pred->right;
+                }
+
+                if (pred->right == nullptr) {
+                    // Make curr the right child of its inorder predecessor
+                    pred->right = curr;
+                    curr = curr->left;
+                } else {
+                    // Restore the tree by removing the link between curr and its predecessor
+                    pred->right = nullptr;
+
+                    // Process the current node and detect swapped nodes
+                    if (prev != nullptr && curr->val < prev->val) {
+                        if (first == nullptr) {
+                            first = prev;
+                            middle = curr;
+                        } else {
+                            last = curr;
+                        }
+                    }
+                    prev = curr;
+                    curr = curr->right;
+                }
+            }
+        }
     }
-    inorder(root->left);
-    if(prev!=nullptr && root->val < prev->val){
-      if(first==nullptr){
-        first=prev;
-        middle=root;
-      }
-      else{
-        last=root;
-      }
-    }
-    prev=root;
-    inorder(root->right);
-  }
+
 public:
     void recoverTree(TreeNode* root) {
-        first=middle=last=nullptr;
-        prev=new TreeNode(INT_MIN);
-        inorder(root);
-        if(first&&last){
-          swap(first->val ,last->val);
-        }
-        else if(first&&middle){
-          swap(first->val,middle->val);
+        first = middle = last = nullptr;
+        prev = new TreeNode(INT_MIN);
+
+        // Use Morris traversal to find swapped nodes
+        morrisTraversal(root);
+
+        // Fix the swapped nodes
+        if (first && last) {
+            swap(first->val, last->val);  // Non-adjacent nodes swapped
+        } else if (first && middle) {
+            swap(first->val, middle->val);  // Adjacent nodes swapped
         }
     }
 };
