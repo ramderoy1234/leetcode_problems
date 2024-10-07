2056db776d16d25d63f2d510807de5f8b9326a85
ramderoy1234 <moodramcharan@gmail.com>
2024-10-07 16:07:09 +0530

    Time: 48 ms (72.58%), Space: 36.6 MB (32.65%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,23 +1,16 @@
 class Solution{ 
-void bfs(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&visited,int drow[], int dcol[]){
+void dfs(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&visited,int drow[], int dcol[]){
   int n=grid.size();
   int m=grid[0].size();
    visited[row][col]=1;
-   queue<pair<int,int>>q;
-   q.push({row,col});
-   while(!q.empty()){
-    int r=q.front().first;
-    int c=q.front().second;
-     q.pop();
-     for(int i=0;i<4;i++){
-       int nrow=drow[i]+r;
-       int ncol=dcol[i]+c;
-       if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol]==1){
-          q.push({nrow,ncol});
-          visited[nrow][ncol]=1;
-       }
-     }
-   }
+  for(int i=0;i<4;i++){
+    int nrow=drow[i]+row;
+    int ncol=dcol[i]+col;
+    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol]==1 ){
+      dfs(nrow,ncol,grid,visited,drow,dcol);
+    }
+  }
+ 
 }
 
 public:
@@ -30,19 +23,19 @@ public:
 
         for(int i=0;i<m;i++){
           if(!visited[0][i] && grid[0][i]==1 ){
-            bfs(0,i,grid,visited,drow,dcol);
+            dfs(0,i,grid,visited,drow,dcol);
           }
           if(!visited[n-1][i] && grid[n-1][i]==1){
-            bfs(n-1,i,grid,visited,drow,dcol);
+            dfs(n-1,i,grid,visited,drow,dcol);
           }
         }
 
         for(int j=0;j<n;j++){
           if(!visited[j][0] && grid[j][0]==1){
-            bfs(j,0,grid,visited,drow,dcol);
+            dfs(j,0,grid,visited,drow,dcol);
           }
           if(!visited[j][m-1] && grid[j][m-1]==1){
-            bfs(j,m-1,grid,visited,drow,dcol);
+            dfs(j,m-1,grid,visited,drow,dcol);
           }
         }
         int cnt=0;
