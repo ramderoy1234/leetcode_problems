04337c69267bc098d8a13bb32839d2246176479f
ramderoy1234 <moodramcharan@gmail.com>
2024-10-09 23:26:17 +0530

    Time: 0 ms (100%), Space: 11.3 MB (34.74%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,20 @@
 class Solution {
-public:
-    void generateSubsets(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
-        // Base case: if index reaches the end of the nums array
-        if (index == nums.size()) {
-            ans.push_back(current); // Add the current subset to the answer
-            return;
-        }
-
-        // Exclude the current element and move to the next
-        generateSubsets(index + 1, nums, current, ans);
-
-        // Include the current element in the subset
-        current.push_back(nums[index]);
-        generateSubsets(index + 1, nums, current, ans);
-
-        // Backtrack: remove the element after recursion
-        current.pop_back();
+  void recursion(int n,vector<int>&nums,vector<int>&curr,vector<vector<int>>&ans){
+    if(n==0){
+      ans.push_back(curr);
+      return ;
     }
-
+    recursion(n-1,nums,curr,ans);
+    curr.push_back(nums[n-1]);
+    recursion(n-1,nums,curr,ans);
+    curr.pop_back();
+  }
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> current;
-        generateSubsets(0, nums, current, ans);  // Start the recursion
+        int n=nums.size();
+        vector<int>curr;
+        vector<vector<int>>ans;
+        recursion(n,nums,curr,ans);
         return ans;
     }
 };
-
