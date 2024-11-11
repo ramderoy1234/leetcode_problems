bbdd329817ef9bf394fc7fe4261e5d5a483c1bd9
ramderoy1234 <moodramcharan@gmail.com>
2024-11-11 19:20:14 +0530

    Time: 0 ms (100%), Space: 13.7 MB (70.92%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -4,18 +4,15 @@ public:
         int n=matrix.size();
         if(n==0) return 0;
         vector<vector<int>>dp=matrix;
+        
         for(int i=n-2;i>=0;i--){
-          for(int j=0;j<n;j++){
-            int down=dp[i+1][j];
-            int leftDia=(j>0)?dp[i+1][j-1]:INT_MAX;
-            int rightDia=(j<n-1)?dp[i+1][j+1]:INT_MAX;
-            dp[i][j]+=min({down,leftDia,rightDia});
-          }
+            for(int j=0;j<n;j++){
+                int down=dp[i+1][j];
+                int leftdia =(j>0) ? dp[i+1][j-1]:INT_MAX;
+                int rightdia=(j<n-1) ? dp[i+1][j+1]:INT_MAX;
+                dp[i][j]+=min({down,leftdia,rightdia});
+            }
         }
-        int mini=INT_MAX;
-        for(int j=0;j<n;j++){
-          mini=min(mini,dp[0][j]);
-        }
-        return mini;
+        return *min_element(dp[0].begin(),dp[0].end());
     }
 };
