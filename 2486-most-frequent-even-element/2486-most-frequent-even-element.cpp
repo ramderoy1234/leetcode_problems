62ee2c01b02a00091c2f924a9eb76b52f98a0a57
ramderoy1234 <moodramcharan@gmail.com>
2024-12-04 22:27:26 +0530

    Time: 12 ms (95.24%), Space: 41.7 MB (89.7%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.cpp
@@ -3,13 +3,12 @@ public:
     int mostFrequentEven(vector<int>& nums) {
         unordered_map<int,int>mp;
         for(auto it:nums){
-           if(it%2==0) mp[it]++;
+            if(it%2==0) mp[it]++;
         }
-        int small_even=0;
-        int maxi=0;
         int ans=-1;
+        int maxi=0;
         for(auto c:mp){
-            if(c.second>maxi || (c.second==maxi &&  c.first < ans )){
+            if(c.second>maxi || ( c.second==maxi && c.first <ans )){
                 maxi=c.second;
                 ans=c.first;
             }
