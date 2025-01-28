8fe820b99d3039726716fd7184e14145f58a018d
ramderoy1234 <moodramcharan@gmail.com>
2025-01-28 23:17:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.26%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -8,29 +8,28 @@ public:
             int mid=(s+e)/2;
             if(nums[mid]==target){
                 ans[0]=mid;
-                e=mid-1;   // early start
-            }
-            else if(nums[mid]>target){
                 e=mid-1;
             }
-            else{
+            else if(nums[mid]<target){
                 s=mid+1;
             }
+            else{
+                e=mid-1;
+            }
         }
-        
         s=0;
         e=nums.size()-1;
         while(s<=e){
             int mid=(s+e)/2;
             if(nums[mid]==target){
                 ans[1]=mid;
-                s=mid+1;  // last occurance
+                s=mid+1;
             }
-            else if(nums[mid]>target){
-                e=mid-1;
+            else if(nums[mid]<target){
+                s=mid+1;
             }
             else{
-                s=mid+1;
+                e=mid-1;
             }
         }
         return ans;
