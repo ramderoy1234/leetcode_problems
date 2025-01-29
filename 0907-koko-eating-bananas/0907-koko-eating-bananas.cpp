32d981adcc1d198806bce60afdb6f9a3481f65c8
ramderoy1234 <moodramcharan@gmail.com>
2025-01-29 22:19:51 +0530

    Time: 11 ms (44.85%), Space: 22.9 MB (75.14%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,30 +1,25 @@
 class Solution {
-public:
-    int check(vector<int>& piles, int h, int mid) {
-        int ans = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            ans += (piles[i] / mid);
-            // If there are leftover bananas in this pile, it takes one more hour to eat them
-            if (piles[i] % mid != 0) {
-                ans++; // Extra hour for the remaining bananas in the current pile
+    int check(vector<int>&piles,int h, int mid){
+        int ans=0;
+        for(int i=0;i<piles.size();i++){
+            ans+=(piles[i]/mid);
+            if(piles[i]%mid!=0){
+                ans++;
             }
         }
-
-        return ans <= h;
+        return ans<=h;
     }
-
+public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size(); 
-        int low = 1; 
-        int high = *max_element(piles.begin(), piles.end()); // Maximum possible speed (the largest pile size)
-
-        while (low < high) {
-            int mid = (low + high)/2;
-            // If Koko can finish eating all bananas with speed `mid` in `h` hours
-            if (check(piles, h, mid)) {
-                high = mid;
-            } else {
-                low = mid + 1; 
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        while(low<high){
+            int mid=(low+high)/2;
+            if(check(piles,h,mid)){
+                high=mid;
+            }
+            else{
+                low=mid+1;
             }
         }
         return low;
