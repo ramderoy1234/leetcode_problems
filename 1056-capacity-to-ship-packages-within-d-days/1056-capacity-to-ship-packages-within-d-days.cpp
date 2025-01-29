fe77c67a4065199d41de159682e44eaf1a74057a
ramderoy1234 <moodramcharan@gmail.com>
2025-01-29 23:43:30 +0530

    Time: 15 ms (36.96%), Space: 34.8 MB (99.44%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,32 +1,31 @@
 class Solution {
-  int find(vector<int>&weights,int cap){
-    int days=1;
-    int load=0;
-    for(int i=0;i<weights.size();i++){
-      if(weights[i]+load > cap){
-        days++;
-        load=weights[i];
-      }
-      else{
-        load+=weights[i];
-      }
+    int find(vector<int>&weights,int mid){
+        int days=1;
+        int load=0;
+        for(int i=0;i<weights.size();i++){
+            if(weights[i]+load > mid){
+                load=weights[i];
+                days++;
+            }
+            else{
+                load+=weights[i];
+            }
+        }
+        return days;
     }
-    return days;
-  }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int n=weights.size();
         int s=*max_element(weights.begin(),weights.end());
         int e=accumulate(weights.begin(),weights.end(),0);
         while(s<=e){
-          int cap=(s+e)/2;
-          int shipcapacity=find(weights,cap);
-          if(shipcapacity<=days){
-            e=cap-1;
-          }
-          else{
-            s=cap+1;
-          }
+            int mid=s+(e-s)/2;
+            int shipCap=find(weights,mid);
+            if(shipCap<=days){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
         }
         return s;
     }
