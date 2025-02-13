796c90bac3b03182453dc69e0ebf202c55c77c3d
ramderoy1234 <moodramcharan@gmail.com>
2025-02-13 12:32:18 +0530

    Time: 27 ms (49.25%), Space: 139.2 MB (61.54%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,36 +1,24 @@
-
 class Solution {
 public:
-    std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k) {
-         int n = nums.size();
-    if (n == 0) return {};
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            // Remove elements out of the current window
+            if (!dq.empty() && dq.front() == i - k) dq.pop_front();
 
-    std::vector<int> left_max(n), right_max(n);
-    
-    // Precompute the max in each block from left to right
-    for (int i = 0; i < n; ++i) {
-        if (i % k == 0) {
-            left_max[i] = nums[i]; // Start of a block
-        } else {
-            left_max[i] = std::max(left_max[i - 1], nums[i]);
+            // Remove smaller elements from the back
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+
+            // Add maximum element of the window to answer
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
         }
-    }
 
-    // Precompute the max in each block from right to left
-    for (int i = n - 1; i >= 0; --i) {
-        if (i == n - 1 || (i + 1) % k == 0) {
-            right_max[i] = nums[i]; // End of a block
-        } else {
-            right_max[i] = std::max(right_max[i + 1], nums[i]);
-        }
-    }
-
-    std::vector<int> sliding_max;
-    for (int i = 0; i <= n - k; ++i) {
-        sliding_max.push_back(std::max(left_max[i + k - 1], right_max[i]));
-    }
-
-    return sliding_max;
+        return ans;
     }
 };
-
