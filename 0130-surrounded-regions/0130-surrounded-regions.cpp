09f08083ff9a401eae3b00c974ad6f1c2bb93417
ramderoy1234 <moodramcharan@gmail.com>
2025-02-18 18:21:25 +0530

    Time: 0 ms (100%), Space: 14.4 MB (61.96%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,16 +1,17 @@
 class Solution {
-  void dfs(int row,int col,vector<vector<char>>& board,vector<vector<int>>&visited,int drow[], int dcol[]){
-    visited[row][col]=1;
-    int n=board.size();
-    int m=board[0].size();
-    for(int i=0;i<4;i++){
-      int nrow=drow[i]+row;
-      int ncol=dcol[i]+col;
-      if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && board[nrow][ncol]=='O' && !visited[nrow][ncol]){
-         dfs(nrow,ncol,board,visited,drow,dcol);
-      }
+    void dfs(int row,int col,vector<vector<char>>&board,vector<vector<int>>&visited,int drow[],int dcol[]){
+        int n=board.size();
+        int m=board[0].size();
+        visited[row][col]=1;
+        for(int i=0;i<4;i++){
+            int nrow=drow[i]+row;
+            int ncol=dcol[i]+col;
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && board[nrow][ncol]=='O'){
+                dfs(nrow,ncol,board,visited,drow,dcol);
+            }
+        }
     }
-  }
+
 public:
     void solve(vector<vector<char>>& board) {
         int n=board.size();
@@ -20,29 +21,29 @@ public:
         int dcol[]={-1,0,1,0};
 
         for(int i=0;i<m;i++){
-          if(!visited[0][i] && board[0][i]=='O'){
-            dfs(0,i,board,visited,drow,dcol);
-          }
-          if(!visited[n-1][i] && board[n-1][i]=='O'){
-            dfs(n-1,i,board,visited,drow,dcol);
-          }
+            if(!visited[0][i] && board[0][i]=='O'){
+                dfs(0,i,board,visited,drow,dcol);
+            }
+            if(!visited[n-1][i] && board[n-1][i]=='O'){
+                dfs(n-1,i,board,visited,drow,dcol);
+            }
         }
 
         for(int j=0;j<n;j++){
-          if(!visited[j][0] && board[j][0]=='O'){
-            dfs(j,0,board,visited,drow,dcol);
-          }
-          if(!visited[j][m-1] && board[j][m-1]=='O'){
-            dfs(j,m-1,board,visited,drow,dcol);
-          }
-        }
-        
-        for(int i=0;i<n;i++){
-          for(int j=0;j<m;j++){
-            if(board[i][j]=='O' && !visited[i][j]){
-              board[i][j]='X';
+            if(!visited[j][0] && board[j][0]=='O'){
+                dfs(j,0,board,visited,drow,dcol);
             }
-          }
+            if(!visited[j][m-1] && board[j][m-1]=='O'){
+                dfs(j,m-1,board,visited,drow,dcol);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && !visited[i][j]){
+                    board[i][j]='X';
+                }
+            }
         }
     }
 };
