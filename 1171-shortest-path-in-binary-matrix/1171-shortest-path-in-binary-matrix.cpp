8dc8b733725ff3bbdb35ee22c530c208f842d402
ramderoy1234 <moodramcharan@gmail.com>
2025-02-20 15:19:35 +0530

    Time: 13 ms (82.08%), Space: 26.2 MB (45.27%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        queue<pair<int, pair<int, int>>> q;  // (length, (row, col))
-    
-        if (grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1; 
-        q.push({1, {0, 0}});
-        visited[0][0] = 1;
-        if (n == 1 && m == 1) return 1;
-        vector<pair<int, int>> directions = {
-            {0, 1}, {0, -1}, {1, 0}, {-1, 0},
-            {1, 1}, {-1, -1}, {1, -1}, {-1, 1}
-        };
-
-        while (!q.empty()) {
-            int length = q.front().first;
-            int row = q.front().second.first;
-            int col = q.front().second.second;
-            q.pop();
-
-            if (row == n - 1 && col == m - 1) {
-                return length;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        q.push({{0,0},1}); // row col lenght;
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
+       int drow[] = {-1, -1, -1, 0, 1, 1, 1, 0};  
+       int dcol[] = {-1, 0, 1, 1, 1, 0, -1, -1};
+       vector<vector<int>>visited(n,vector<int>(m,0));
+       visited[0][0]=1;
+       while(!q.empty()){
+          int row=q.front().first.first;
+          int col=q.front().first.second;
+          int lenght=q.front().second;
+          if(row==n-1 && col==m-1){
+            return lenght;
+          }
+          q.pop();
+          for(int i=0;i<8;i++){
+            int nrow=drow[i]+row;
+            int ncol=dcol[i]+col;
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !visited[nrow][ncol] && grid[nrow][ncol]==0 ){
+                q.push({{nrow,ncol},lenght+1});
+                visited[nrow][ncol]=1;
             }
-            for (auto& dir : directions) {
-                int nrow = row + dir.first;
-                int ncol = col + dir.second;
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-                    && !visited[nrow][ncol] && grid[nrow][ncol] == 0) {
-                    q.push({length + 1, {nrow, ncol}});
-                    visited[nrow][ncol] = 1;
-                }
-            }
-        }
-        return -1;
+          }
+       }
+       return -1;
     }
 };
