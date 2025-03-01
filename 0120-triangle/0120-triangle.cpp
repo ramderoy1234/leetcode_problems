c540374b5f35c391f00faa21476de0a5426de015
ramderoy1234 <moodramcharan@gmail.com>
2025-03-01 12:52:11 +0530

    Time: 0 ms (100%), Space: 13.3 MB (27.26%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,14 +1,15 @@
 class Solution {
-int recursion(int n,int m,vector<vector<int>>&tri,vector<vector<int>>&dp){
-  if(n==tri.size()-1) return tri[n][m];
-  if(dp[n][m]!=-1) return dp[n][m];
-  int right=recursion(n+1,m,tri,dp);
-  int down=recursion(n+1,m+1,tri,dp);
-  return dp[n][m]=tri[n][m]+min(right,down);
-}  
+  int recursion(int n,int m,vector<vector<int>>&triangle,vector<vector<int>>&dp){
+    if(n==triangle.size()-1) return triangle[n][m];
+    if(dp[n][m]!=-1) return dp[n][m];
+    int left=recursion(n+1,m,triangle,dp);
+    int right=recursion(n+1,m+1,triangle,dp);
+    return dp[n][m]=triangle[n][m]+min(left,right);
+  }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
+        int m=triangle[0].size();
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
         return recursion(0,0,triangle,dp);
     }
