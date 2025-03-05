085f08b4d9ec004b8a7ff7a271fdd15a3160e7e2
ramderoy1234 <moodramcharan@gmail.com>
2025-03-05 12:14:28 +0530

    Time: 608 ms (5.01%), Space: 229.7 MB (6.01%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    int recursion(int index, int buy, int cnt, vector<int>& prices, vector<vector<vector<int>>>& dp, int k) {
-        if (index >= prices.size() || cnt == k) return 0;
-        if (dp[index][cnt][buy] != -1) return dp[index][cnt][buy];
-        int notpick = recursion(index + 1, buy, cnt, prices, dp, k);
-
-        if (buy) {
-            int buyStock = -prices[index] + recursion(index + 1, 0, cnt, prices, dp, k);
-            dp[index][cnt][buy] = max(buyStock, notpick);
-        } else {
-            int sellStock = prices[index] + recursion(index + 1, 1, cnt + 1, prices, dp, k);
-            dp[index][cnt][buy] = max(sellStock, notpick);
-        }
-        return dp[index][cnt][buy];
+  int recursion(int index,int buy,int cnt,int k,vector<int>&prices,vector<vector<vector<int>>>&dp){
+    if(index>=prices.size() || cnt==k) return 0;
+    if(dp[index][cnt][buy]!=-1) return dp[index][cnt][buy];
+    int notPick=recursion(index+1,buy,cnt,k,prices,dp);
+    if(buy){
+      int buy_stock=-prices[index]+recursion(index+1,0,cnt,k,prices,dp);
+      dp[index][cnt][buy]=max(notPick,buy_stock);
     }
-
+    else{
+      int sell=prices[index]+recursion(index+1,1,cnt+1,k,prices,dp);
+      dp[index][cnt][buy]=max(notPick,sell);
+    }
+    return dp[index][cnt][buy];
+  }
 public:
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k + 1, vector<int>(2, -1))); 
-        return recursion(0, 1, 0, prices, dp, k);
+        int n=prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(n+1,vector<int>(3,-1)));
+        
+        return recursion(0,1,0,k,prices,dp);
     }
 };
