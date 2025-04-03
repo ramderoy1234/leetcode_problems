165522e355e9399c832fd82dae38bb109b8d08fe
ramderoy1234 <moodramcharan@gmail.com>
2025-04-03 22:02:23 +0530

    Time: 0 ms (100%), Space: 15 MB (43.3%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,21 +1,18 @@
-
 class Solution {
-  void dfs(TreeNode*root,int level,int &maxi,vector<int>&ans){
-    if(root==nullptr) return ;
-    if(maxi<level){
-      maxi=level;
+  void dfs(TreeNode*root, int level, int &maxi,vector<int>&ans){
+     if(root==nullptr) return ;
+     if(maxi<level){
       ans.push_back(root->val);
-    }
-    dfs(root->right,level+1,maxi,ans);
-    dfs(root->left,level+1,maxi,ans);
-    
+      maxi=level;
+     }
+     dfs(root->right,level+1,maxi,ans);
+     dfs(root->left,level+1,maxi,ans);
   }
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int>ans;
-        if(root==nullptr) return ans;
         int maxi=INT_MIN;
         int level=0;
+        vector<int>ans;
         dfs(root,level,maxi,ans);
         return ans;
     }
