5b7d4878127f93caaaf7563e09cd822f46e7ceb1
ramderoy1234 <moodramcharan@gmail.com>
2025-04-05 15:22:19 +0530

    Time: 8 ms (24.18%), Space: 39 MB (20.71%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,16 +1,16 @@
 
 class Solution {
-    bool check(TreeNode*root, unordered_set<int>&mp, int k){
-        if(root==nullptr) return false;
-        if(mp.count(k-root->val)){
-            return true;
-        }
-        mp.insert(root->val);
-        return check(root->left,mp,k) || check(root->right,mp,k);
+  bool check(TreeNode*root,int k, unordered_set<int>&mp){
+    if(root==nullptr) return false;
+    if(mp.count(k-root->val)){
+      return true;
     }
+    mp.insert(root->val);
+   return check(root->left,k,mp) || check(root->right,k,mp);
+  }
 public:
     bool findTarget(TreeNode* root, int k) {
         unordered_set<int>mp;
-        return check(root,mp,k);
+        return check(root,k,mp);
     }
 };
