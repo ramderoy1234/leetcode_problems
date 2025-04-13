40da70879cb238689e33c31ac23b92c8f1540344
ramderoy1234 <moodramcharan@gmail.com>
2025-04-13 21:28:52 +0530

    Time: 0 ms (100%), Space: 9 MB (87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,25 @@
 class Solution {
-    void recursion(string digits,int i, string output, vector<string>&ans,string mapping[]){
-        if(i>=digits.size()){
-            ans.push_back(output);
-            return ;
-        }
-        int num=digits[i]-'0';
-        string value=mapping[num];
-        for(int j=0;j<value.size();j++){
-            output.push_back(value[j]);
-            recursion(digits,i+1,output,ans,mapping);
-            output.pop_back();
-        }
+  void recursion(int i, string &digits,string &curr,vector<string>&ans, string mapping[]){
+    if(i>=digits.size()){
+      ans.push_back(curr);
+      return ;
     }
+    int num=digits[i]-'0';
+    string value=mapping[num];
+    for(int j=0;j<value.size();j++){
+      curr.push_back(value[j]);
+      recursion(i+1,digits,curr,ans,mapping);
+      curr.pop_back();
+    }
+
+  }
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string>ans;
+       vector<string>ans;
         if(digits.size()==0) return ans;
+        string curr;
         string mapping[10] = {"",    "",    "abc",  "def", "ghi","jkl", "mno", "pqrs", "tuv", "wxyz"};
-        string output;
-        recursion(digits,0,output,ans,mapping);
+        recursion(0,digits,curr,ans,mapping);
         return ans;
     }
 };
