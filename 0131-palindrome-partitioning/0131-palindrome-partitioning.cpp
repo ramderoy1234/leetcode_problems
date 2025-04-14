4b2e149200131e84aacc961531d885c1f37b1903
ramderoy1234 <moodramcharan@gmail.com>
2025-04-14 11:57:46 +0530

    Time: 30 ms (90.2%), Space: 53.5 MB (84.57%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,26 +1,26 @@
 class Solution {
-    bool palindrome(int i,int j,string s){
-        while(i<j){
-            if(s[i++]!=s[j--]) return false;
-        }
-        return true;
+  bool palindrome(int i,int j,string &s){
+    while(i<j){
+      if(s[i++]!=s[j--]) return false;
     }
-    void recursion(int st,string s,vector<string>&path,vector<vector<string>>&ans){
-        if(st==s.size()){
-            ans.push_back(path);
-            return ;
-        }
-        for(int i=st;i<s.size();i++){
-            if(palindrome(st,i,s)){
-                path.push_back(s.substr(st,i-st+1));
-                recursion(i+1,s,path,ans);
-                path.pop_back();
-            }
-        }
+    return true;
+  }
+  void recursion(int start,string &s,vector<string>&path,vector<vector<string>>&ans){
+    if(start==s.size()){
+      ans.push_back(path);
+      return ;
     }
+    for(int i=start;i<s.size();i++){
+      if(palindrome(start,i,s)){
+        path.push_back(s.substr(start,i-start+1));
+        recursion(i+1,s,path,ans);
+        path.pop_back();
+      }
+    }
+  }
 public:
     vector<vector<string>> partition(string s) {
-        vector<string>path;
+        vector<string> path;
         vector<vector<string>>ans;
         recursion(0,s,path,ans);
         return ans;
