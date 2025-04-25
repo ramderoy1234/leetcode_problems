f76b61a300374ea44a603b208d32e6cd100641f6
ramderoy1234 <moodramcharan@gmail.com>
2025-04-25 21:36:16 +0530

    Time: 0 ms (100%), Space: 16.8 MB (89.89%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -1,28 +1,31 @@
 class Solution {
-  vector<TreeNode*>solve(int start,int end){
-    if(start>end) return {nullptr};
-    if(start==end){
-      TreeNode*root=new TreeNode(start);
-      return {root};
-    }
-    vector<TreeNode*>ans;
-    for(int i=start;i<=end;i++){
-      vector<TreeNode*>left=solve(start,i-1);
-      vector<TreeNode*>right=solve(i+1,end);
-      for(auto &leftBT:left){
-        for(auto &rightBT:right){
-          TreeNode*root=new TreeNode(i);
-          root->left=leftBT;
-          root->right=rightBT;
-          ans.push_back(root);
+    map<pair<int, int>, vector<TreeNode*>>dp;
+    vector<TreeNode*> solve(int start, int end) {
+        if (start > end) return {nullptr};
+
+        if (dp.count({start, end})) return dp[{start, end}];
+
+        vector<TreeNode*> ans;
+        for (int i = start; i <= end; ++i) {
+            vector<TreeNode*> left = solve(start, i - 1);
+            vector<TreeNode*> right = solve(i + 1, end);
+
+            for (auto& leftBT : left) {
+                for (auto& rightBT : right) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = leftBT;
+                    root->right = rightBT;
+                    ans.push_back(root);
+                }
+            }
         }
-      }
+
+        return dp[{start, end}] = ans;
     }
-    return ans;
-  }
+
 public:
     vector<TreeNode*> generateTrees(int n) {
-        if(n==0) return {};
-        return solve(1,n);
+        if (n == 0) return {};
+        return solve(1, n);
     }
 };
