668ebfa829cd34ce38eed7e087762243be01900c
ramderoy1234 <moodramcharan@gmail.com>
2025-06-11 18:16:28 +0530

    Time: 3 ms (38.67%), Space: 10.2 MB (99.87%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
-    void recursion(int start,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
-        ans.push_back(ds);
-        for(int i=start;i<nums.size();i++){
-            if(i>start && nums[i]==nums[i-1]) continue;
-            ds.push_back(nums[i]);
-            recursion(i+1,nums,ds,ans);
-            ds.pop_back();
-        }
+  void recursion(int start,vector<int>&nums,vector<int>&curr,vector<vector<int>>&ans){
+    ans.push_back(curr);
+    for(int i=start;i<nums.size();i++){
+      if(i> start && nums[i]==nums[i-1]) continue;
+      curr.push_back(nums[i]);
+      recursion(i+1,nums,curr,ans);
+      curr.pop_back();
     }
+  }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>curr;
         sort(nums.begin(),nums.end());
-        vector<int>ds;
         vector<vector<int>>ans;
-        recursion(0,nums,ds,ans);
+        recursion(0,nums,curr,ans);
         return ans;
     }
 };
