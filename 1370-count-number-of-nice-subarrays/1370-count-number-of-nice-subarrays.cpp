9dd346933882a82b645cf1242e653b1bced81223
ramderoy1234 <moodramcharan@gmail.com>
2025-06-14 12:30:16 +0530

    Time: 85 ms (12.1%), Space: 94.4 MB (14.47%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -6,13 +6,15 @@ public:
           if(nums[i]&1) nums[i]=1;
           else nums[i]=0;
         }
-        unordered_map<int,int>mp;
         int sum=0;
         int cnt=0;
+        unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++){
           sum+=nums[i];
           if(sum==k) cnt++;
-          if(mp.find(sum-k)!=mp.end()) cnt+=mp[sum-k];
+          if(mp.find(sum-k)!=mp.end()){
+            cnt+=mp[sum-k];
+          }
           mp[sum]++;
         }
         return cnt;
