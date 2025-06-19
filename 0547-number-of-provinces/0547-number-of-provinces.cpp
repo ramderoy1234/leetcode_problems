dab0d873aba2447675cee55c4ded0762333731fa
ramderoy1234 <moodramcharan@gmail.com>
2025-06-19 17:03:51 +0530

    Time: 0 ms (100%), Space: 20.4 MB (41.22%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,51 +1,29 @@
-class  DisjointSet{
-   public:
-   vector<int>rank,parents;
-     DisjointSet(int n){
-     rank.resize(n,0);
-     parents.resize(n,0);
-     for(int i=0;i<n;i++){
-      parents[i]=i;
-     }
-    }
-    int findPar(int node){
-      if(node==parents[node]){
-        return node;
-      }
-      return parents[node]=findPar(parents[node]);
-    }
-    void unionByrank(int u, int v) {
-     int ult_u = findPar(u);
-     int ult_v = findPar(v);
-     if (ult_u == ult_v) return;
-
-     if (rank[ult_u] > rank[ult_v]) {  
-       parents[ult_v] = ult_u; 
-     } 
-     else if (rank[ult_v] > rank[ult_u]) {  
-       parents[ult_u] = ult_v;  
-     } 
-     else {  
-       parents[ult_v] = ult_u;  
-       rank[ult_u]++; 
-     }
-   }
-};
 class Solution {
+  void dfs(int node,vector<vector<int>>&adj,vector<int>&visited){
+    visited[node]=1;
+    for(auto it:adj[node]){
+      if(!visited[it]){
+        dfs(it,adj,visited);
+        visited[it]=1;
+      }
+    }
+  }
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-      int n=isConnected.size();
-        DisjointSet ds(n);
-        int cnt=0;
+        int n=isConnected.size();
+        vector<vector<int>>adj(n);
+        vector<int>visited(n,0);
         for(int i=0;i<n;i++){
           for(int j=0;j<n;j++){
             if(isConnected[i][j]==1){
-              ds.unionByrank(i,j);
+              adj[i].push_back(j);
             }
           }
         }
+        int cnt=0;
         for(int i=0;i<n;i++){
-          if(ds.parents[i]==i){
+          if(!visited[i]){
+            dfs(i,adj,visited);
             cnt++;
           }
         }
