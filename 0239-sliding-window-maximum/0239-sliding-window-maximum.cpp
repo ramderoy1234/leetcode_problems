df83219510ae4a8f842720627a771a12781eb9ce
ramderoy1234 <moodramcharan@gmail.com>
2025-07-10 12:39:34 +0530

    Time: 19 ms (76.78%), Space: 139.2 MB (36.09%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -6,10 +6,11 @@ public:
         deque<int>dq;
         for(int i=0;i<n;i++){
 
+         //  Remove indices that are out of current window
           while(!dq.empty() &&dq.front() <= i-k){
             dq.pop_front();
           }
-
+          //  Remove elements from back which are smaller than current element
           while(!dq.empty() && nums[i]> nums[dq.back()] ){
              dq.pop_back();
           }
@@ -18,7 +19,7 @@ public:
           if(i>=k-1){
              ans.push_back(nums[dq.front()]);
           }
-
+          
         }
         return ans;
     }
