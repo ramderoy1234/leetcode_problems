da4ec5c9d171f26b54faa2df49299eefd6841a87
ramderoy1234 <moodramcharan@gmail.com>
2025-07-20 16:56:06 +0530

    Time: 4 ms (43.41%), Space: 13.9 MB (90.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
-  void recursion(int i,vector<int>& candidates, int target,vector<int>&curr,vector<vector<int>>&ans){
-    if(i==candidates.size()){
-      if(target==0) ans.push_back(curr);
+  void subsets(int i,vector<int>&candi,int target,vector<int>&curr,vector<vector<int>>&ans){
+    if(i==candi.size()){
+      if(target==0)
+        ans.push_back(curr);
       return ;
     }
-
-    if(candidates[i]<=target){
-      curr.push_back(candidates[i]);
-      recursion(i,candidates,target-candidates[i],curr,ans);
+    if(candi[i]<=target){
+      curr.push_back(candi[i]);
+      subsets(i,candi,target-candi[i],curr,ans);
       curr.pop_back();
     }
-    recursion(i+1,candidates,target,curr,ans);
+    subsets(i+1,candi,target,curr,ans);
   }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int>curr;
         vector<vector<int>>ans;
-        recursion(0,candidates,target,curr,ans);
+        subsets(0,candidates,target,curr,ans);
         return ans;
     }
 };
