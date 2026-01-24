6e2147a311eed67308b9b3704c8235e99bec891b
Ram charan <moodramcharan@gmail.com>
2026-01-24 22:24:19 +0530

    Time: 44 ms (55.14%), Space: 45.4 MB (69.79%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,15 +4,16 @@ public:
         int n=nums.size();
         unordered_map<int,int>mp;
         mp[0]=1;
-        int sum=0;
         int cnt=0;
+        int sum=0;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(mp.find(sum-k)!=mp.end()){
-                cnt+=mp[sum-k];
-            }
-            mp[sum]++;
+          sum+=nums[i];
+          if(mp.find(sum-k)!=mp.end()){
+            cnt+=mp[sum-k];
+          }
+          mp[sum]++;
         }
         return cnt;
     }
 };
+
