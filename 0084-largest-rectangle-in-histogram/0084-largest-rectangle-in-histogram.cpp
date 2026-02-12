dcb768121a7f6a7cd1274c70575fbf07fa92d0cd
Ram charan <moodramcharan@gmail.com>
2026-02-12 23:01:38 +0530

    Time: 40 ms (13.42%), Space: 87.7 MB (14.73%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+vector<int>NextsmallerElement(vector<int>&arr,int n){
+  stack<int>st;
+  vector<int>ans(n,n);
+  for(int i=n-1;i>=0;i--){
+    while(!st.empty() && arr[i]<=arr[st.top()]){
+      st.pop();
+    }
+    if(!st.empty()) ans[i]=st.top();
+    st.push(i);
+  }
+  return ans;
+}
+vector<int>PreviousSmallerElement(vector<int>&arr,int n){
+  stack<int>st;
+  vector<int>ans(n,-1);
+  for(int i=0;i<n;i++){
+    while(!st.empty() && arr[i]<arr[st.top()]){
+      st.pop();
+    }
+    if(!st.empty()) ans[i]=st.top();
+    st.push(i);
+  }
+  return ans;
+}
     int largestRectangleArea(vector<int>& histo) {
-        stack<int> st;
-        int maxA = 0;
-        int n = histo.size();
-        for(int i = 0; i <= n; i++) {
-            while(!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
-                int height = histo[st.top()];
-                st.pop();
-                int width;
-                if(st.empty()) width = i;
-                else width = i - st.top() - 1;
-                maxA = max(maxA, width * height);
-            }
-            st.push(i);
-        }
-        return maxA;
+      int n=histo.size();
+      vector<int>NSE=NextsmallerElement(histo,n);
+      vector<int>PSE=PreviousSmallerElement(histo,n);
+      int area=INT_MIN;
+      for(int i=0;i<n;i++){
+        int lenght=histo[i];
+        int breadth=NSE[i]-PSE[i]-1;
+        int newArea=lenght*breadth;
+        area=max(newArea,area);
+      }
+      return area;
     }
 };
